fec6cd0e79d2de863794067ce7377aec1c427923
sumaiyahlee <sumaiyahlee.sl@gmail.com>
2019-08-11 17:35:31 -0700

    Setting up main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import webapp2
+import jinja2
+import os
+import json
+from urllib import urlencode
+from google.appengine.api import urlfetch
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+
+
+
+
+
+app = webapp2.WSGIApplication([
+
+], debug=True)
