fb4834e2b822febe955cba0af9598dd77f311337
ND <nd@keckmac5.local>
2019-08-12 12:31:12 -0700

    Added question css

--- main.py
+++ main.py
@@ -32,7 +32,11 @@ class HomeHandler(webapp2.RequestHandler):
             
             }))
 
+class QuestionsHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/questions.html')
 
 app = webapp2.WSGIApplication([
     ('/', HomeHandler),
+    ('/questions.html', QuestionsHandler),
 ], debug=True)
