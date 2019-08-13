92e424f7464bcd5671ed510c3f7efb69a7d9b977
ND <nd@keckmac5.local>
2019-08-12 17:11:16 -0700

    Changed models py

--- models.py
+++ models.py
@@ -1,11 +1,10 @@
 class User(ndb.Model):
     username = ndb.StringProperty(required = True)
-    email = ndb.StringProperty(required = True)
-    preferences = ndb.StringProperty(required = True, repeated = True)
-    genre = ndb.StringProperty(required = True, repeated = True)
+    moods = ndb.StringProperty(required = True, repeated = True)
+    genres = ndb.StringProperty(required = True, repeated = True)
+    preferredsize = ndb.IntegerProperty(required = True)
+    playlist = ndb.KeyProperty(Playlist, repeated = True)
 
 
-class Song(ndb.Model):
-    artist =  ndb.StringProperty(required = True)
-    title = ndb.StringProperty(required = True)
-    length = ndb.IntegerProperty(required = True)
+class Playlist(ndb.Model):
+    songs = mdb.StringProperty(required = True, repeated = True)
