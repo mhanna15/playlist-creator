cc332697c979eab5ad45356f3ccbd480f48f7209
mhanna15 <minaghanna@gmail.com>
2019-08-15 10:15:53 -0700

    added js to main page

---
+++ scripts/home.js
@@ -0,0 +1,27 @@
+const DELAY = 500;
+
+function layoutTitle() {
+  const instru = document.querySelector("#instru")
+  const mental = document.querySelector("#mental")
+  const instruString = "Instru"
+  const mentalString = "mental"
+
+  spellout(instru, instruString, 0)
+  spellout(mental, mentalString, (DELAY * instruString.length))
+}
+
+function spellout(element, string, startDelay) {
+  for (let i in string) {
+    const char = string[i]
+    function addChar() {
+      element.textContent = element.textContent + char;
+    }
+    setTimeout(addChar, startDelay + (DELAY * i))
+  }
+}
+
+
+
+window.onload = function() {
+  layoutTitle()
+}
