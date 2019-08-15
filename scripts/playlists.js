8ed79ae7b06d90be7dae8c658313c73dd4accc14
sumaiyahlee <sumaiyahlee.sl@gmail.com>
2019-08-15 09:23:46 -0700

    Fixed playlist page

--- scripts/playlists.js
+++ scripts/playlists.js
@@ -24,3 +24,48 @@ function addSong(url) {
     });
 
   }
+
+// // favorite icon
+// function hover(element) {
+//     let sign = element.parentNode.id.toLowerCase();
+//     let goldStringSign = `../media/gold-png/${sign}-gold.png`;
+//     element.setAttribute('src', goldStringSign);
+//     element.previousElementSibling.style.textShadow = "#FFDD20 0px 0px 3px";
+//   }
+  
+//   function unhover(element) {
+//     let sign = element.parentNode.id.toLowerCase();
+//     let stringSign = `../media/png/${sign}.png`;
+//     element.setAttribute('src', stringSign);
+//     element.previousElementSibling.style.textShadow = "none";
+//   }
+
+//   function click(element) {
+//     let sign = element.parentNode.id.toLowerCase();
+//     let goldStringSign = `../media/gold-png/${sign}-gold.png`;
+//     element.setAttribute('src', goldStringSign);
+//     element.previousElementSibling.style.textShadow = "#FFDD20 0px 0px 3px";
+//   }
+
+// // trash icon
+// function hover(element) {
+//     let sign = element.parentNode.id.toLowerCase();
+//     let goldStringSign = `../media/gold-png/${sign}-gold.png`;
+//     element.setAttribute('src', goldStringSign);
+//     element.previousElementSibling.style.textShadow = "#FFDD20 0px 0px 3px";
+//   }
+  
+//   function unhover(element) {
+//     let sign = element.parentNode.id.toLowerCase();
+//     let stringSign = `../media/png/${sign}.png`;
+//     element.setAttribute('src', stringSign);
+//     element.previousElementSibling.style.textShadow = "none";
+//   }
+
+//   function click(element) {
+//     let sign = element.parentNode.id.toLowerCase();
+//     let goldStringSign = `../media/gold-png/${sign}-gold.png`;
+//     element.setAttribute('src', goldStringSign);
+//     element.previousElementSibling.style.textShadow = "#FFDD20 0px 0px 3px";
+//   }
+  
